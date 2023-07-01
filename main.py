d436d7d5a29d9a6198c173cdbbb5a210b699d457
lucastrschneider <lucastrschneider@gmail.com>
2023-07-01 17:19:33 +0200

    ♻️ Check if numpy was installed too

--- main.py
+++ main.py
@@ -3,7 +3,13 @@
 import requests
 
 def main() -> None:
-    print(requests.__version__)
+    print(f"requests=={requests.__version__} installed")
+
+    try:
+        import numpy
+        print(f"numpy=={numpy.__version__} installed")
+    except:
+        print("numpy not installed")
 
 if __name__ == "__main__":
     main()
